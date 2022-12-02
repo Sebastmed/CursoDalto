26d7b2cedc48f65aa11bb6a5ab7dd719dde2fd54
Sebastián <Usuario@Equipo-PC>
2022-12-01 21:46:39 -0300

    alo

--- 3. JS Nivel 1/Capitulo 3/Ejercicios/Ejercicio3.js
+++ 3. JS Nivel 1/Capitulo 3/Ejercicios/Ejercicio3.js
@@ -206,22 +206,3 @@
 
 
 
-
-function esPrimo(numero) {
-    // Devuelve "true" si "numero" es primo
-    // De lo contrario devuelve "falso"
-    // Pista: un número primo solo es divisible por sí mismo y por 1
-    // Pista 2: Puedes resolverlo usando un bucle `for`
-    // Nota: Los números 0 y 1 NO son considerados números primos
-    if(numero / numero === 1 && numero / 1 === numero && numero !== 0 && numero !== 1){
-      return true
-    }
-    else {
-      return false
-    }
-  }
-  
-
-  resultado = esPrimo (4)
-
-  document.write(resultado)
